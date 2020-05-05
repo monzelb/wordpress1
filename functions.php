7bc3deccc3c005ace9023f56c132b624d8d9e195
Brett Monzel <monzelb@gmail.com>
2020-05-05 11:22:42 -0700

    make styles dynamic

--- functions.php
+++ functions.php
@@ -1,7 +1,10 @@
 <?php
 
 function wordpress1_register_styles(){
-	wp_enqueue_style('wordpress1-main-styles', get_template_directory_uri() . "/style.css", array(), '1.0', 'all');
+	$version = wp_get_theme()->get('Version');
+	wp_enqueue_style('wordpress1-main-styles', get_template_directory_uri() . "/style.css", array('wordpress1-bootstrap'), $version, 'all');
+	wp_enqueue_style('wordpress1-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
+	wp_enqueue_style('wordpress1-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
 }
 
 add_action( 'wp_enqueue_scripts', 'wordpress1_register_styles')
